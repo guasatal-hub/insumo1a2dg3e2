4c7cdc1bbdf5b0ce97f65c10bfd75c3a375ab67e
guasatald <guasatald@gmail.com>
2025-11-10 19:19:27 +0000

    📦-build: Empaquetado o dependencias - se hace la instalacion de Nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
-
 export default function RootLayout() {
-  return <Stack />;
+ return(
+<Stack screenOptions={{headerShown: false}}>
+<Stack.Screen name="index" options={{headerShown: false}}/>
+<Stack.Screen name="pokedex" options={{headerShown: false}}/>
+</Stack>
+ )
 }
